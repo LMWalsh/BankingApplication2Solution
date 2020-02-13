6da20efab51ed40a8931bdbe0a74f493682a176c
max-student <max-student@maxtrain.com>
2020-02-12 21:12:25 -0500

    Add project files.

---
+++ BankingApplication2Project/Program.cs
@@ -0,0 +1,14 @@
+﻿using BankingLibrary2;
+using System;
+
+namespace BankingApplication2Project {
+    class Program {
+        static void Main(string[] args) {
+
+            var chk = new Account();
+                chk.Deposit(100);
+                chk.Withdraw(75);
+
+        }
+    }
+}
